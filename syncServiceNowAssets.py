91db3992c7de8add3cc0c03101e19447e3d32031
Brian Torres-Gil <btorres-gil@paloaltonetworks.com>
2015-04-17 08:41:55 -0700

    Better logging, comments, and removed extra code

--- syncServiceNowAssets.py
+++ syncServiceNowAssets.py
@@ -45,8 +45,7 @@ from pan.xapi import PanXapiError
 
 def retrieve_servicenow_asset_list(sn_username, sn_password, sn_fqdn):
     logging.info("Retrieving assets from ServiceNow")
-    #TODO: sanitize fqdn format
-    url = "https://%s/cmdb_ci_server_list.do?JSON&sysparm_action=getRecords" % sn_fqdn
+    url = "https://%s/cmdb_ci_server_list.do?JSON&sysparm_action=getRecords" % (sn_fqdn,)
 
     password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
     password_manager.add_password(None, url, sn_username, sn_password)
@@ -59,14 +58,13 @@ def retrieve_servicenow_asset_list(sn_username, sn_password, sn_fqdn):
     try:
         handler = urllib2.urlopen(request)
     except urllib2.HTTPError as e:
-        logging.error('can\'t read assets from ServiceNow: urllib2: %s' % e.code)
+        logging.error("can't read assets from ServiceNow: urllib2: %s" % (e.code,))
         sys.exit(1)
 
     return handler.read()  # return JSON text
 
 
 def parse_servicenow_assets(json_assets):
-    #TODO: verify it is JSON formatted or handle error on loads() function
     assets = json.loads(json_assets)
     assets = assets['records']
     logging.info("%d assets parsed from ServiceNow" % len(assets))
@@ -99,68 +97,19 @@ def parse_servicenow_assets(json_assets):
     return interesting_assets
 
 
-def sync_address_objects(device, objects, no_delete=False):
-    logging.info("Begin sync with firewall/panorama address objects")
-    # remove any objects that aren't in ServiceNow
-    try:
-        if not no_delete:
-            logging.info("Delete is allowed. Getting current address objects "
-                          "from firewall/panorama for comparison")
-
-            current_address_objects = device.get_all_address_objects()
-
-            if current_address_objects and 'entry' in current_address_objects['address']:
-                current_address_objects = current_address_objects['address']['entry']
-
-                for fw_address in current_address_objects:
-                    # look for firewall objects of the form used
-                    # by ServiceNow sync
-                    match = re.match(
-                        r"^SN_(.*)_((?:[0-9]{1,3}\.){3}[0-9]{1,3})$",
-                        fw_address['name']
-                    )
-                    if match:
-                        hostname = match.group(1)
-                        ip = match.group(2)
-                        # if the firewall's address object is not in the
-                        # ServiceNow asset list, then remove the object from
-                        # the firewall
-                        object_in_servicenow = next((sn_object for sn_object in objects if (sn_object['host_name'] == hostname and sn_object['ip_address'] == ip)), None)
-                        if not object_in_servicenow:
-                            logging.info("Removing address object on firewall/panorama: %s", fw_address['name'])
-                            device.delete_address_object(fw_address['name'])
-
-        # add/update objects on firewall/panorama
-        for address in objects:
-            name = "SN_%s_%s" % (address['host_name'], address['ip_address'])
-            description = "Synced from ServiceNow asset: %s" % address['name']
-            #TODO: verify format of ip_address
-            ip_address = address['ip_address']
-            logging.info("Adding/updating address object in firewall/panorama: %s" % name)
-            device.add_address_object(name, ip_address, description)
-
-    except PanXapiError as msg:
-        logging.error('pan.xapi.PanXapi:%s' % msg)
-        sys.exit(1)
-
-
-def sync_dynamic_address_objects(device, objects, no_delete=False):
-    # servicenow tags that could be used in address objects:
-    #  os
-    #  sys_class_name
-    logging.info("Begin sync with firewall/panorama dynamic address objects")
+def sync_dynamic_address_groups(device, objects, no_delete=False):
 
     reg_list = []
     unreg_list = []
 
     if not no_delete:
-        logging.info("Delete is allowed. Getting current dynamic addresses "
-                      "from firewall/panorama for comparison")
+        logging.info("Getting current tags from firewall for comparison")
         current_tags = device.get_all_registered_addresses()
         for reg_address in current_tags:
             ip, tag = reg_address
             tags_in_servicenow = next((sn_object for sn_object in objects if (sn_object['ip_address'] == ip and tag in sn_object.values())), None)
             if not tags_in_servicenow:
+                logging.debug("Found tag to delete: %s" % (reg_address,))
                 unreg_list.append(reg_address)
 
     for address in objects:
@@ -168,6 +117,7 @@ def sync_dynamic_address_objects(device, objects, no_delete=False):
         reg_list.append((address['ip_address'], "sn.type."+address['superclass']))
         reg_list.append((address['ip_address'], "sn.os."+address['os']))
 
+    logging.info("Begin sync with firewall dynamic address groups")
     device.update_dynamic_addresses(reg_list, unreg_list)
 
 
@@ -212,6 +162,7 @@ def main():
     else:
         args.verbose = 0
 
+
     # get the list of assets from ServiceNow
     assets_json = retrieve_servicenow_asset_list(args.sn_creds.split(':')[0],
                                                 args.sn_creds.split(':')[1],
@@ -224,15 +175,15 @@ def main():
     from pprint import pformat
     logging.debug("Assets found:  \n%s" % (pformat(interesting_assets),))
 
-    logging.info("Preparing connection to firewall/panorama at %s" % args.fw_hostname)
-    device = device.PanDevice(args.fw_hostname,
-                                 args.fw_port,
-                                 args.fw_creds.split(':')[0],
-                                 args.fw_creds.split(':')[1]
-                                 )
+    logging.info("Preparing connection to firewall at %s" % args.fw_hostname)
+    fw = device.PanDevice(args.fw_hostname,
+                          args.fw_port,
+                          args.fw_creds.split(':')[0],
+                          args.fw_creds.split(':')[1]
+                          )
 
-    #sync_address_objects(device, interesting_assets, args.no_delete)
-    sync_dynamic_address_objects(device, interesting_assets, args.no_delete)
+    sync_dynamic_address_groups(fw, interesting_assets, args.no_delete)
+
 
 # Call the main() function to begin the program if not
 # loaded as a module.
